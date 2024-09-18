386c1496d20d285dd01aadc35938ce848cb6855a
pkralj <patrikkralj97@gmail.com>
2024-09-18 12:40:21 +0200

    Adjusted button styling to match rest of the app

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c706eeb95763ee0dbc7c6ae20871169d66d61a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51cc40fdf1feeee452723b434daa1e48883648d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
