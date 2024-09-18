e51cc40fdf1feeee452723b434daa1e48883648d
pkralj <patrikkralj97@gmail.com>
2024-09-18 10:46:05 +0200

    Added field and error message clearing when switching between login and register

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74588a7d7979690b6fdccaf578625441b0de225a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c706eeb95763ee0dbc7c6ae20871169d66d61a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
